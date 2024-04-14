bdaddf71978d1a81843bbd0c1c627bdba91668be
Zekasheva Anastasia <nastya.zekasheva.05@gmail.com>
2024-04-15 02:12:36 +0300

    added calculator with gui

---
+++ CalculatorWinFormApp/Calculator/Calculator.cs
@@ -0,0 +1,238 @@
+﻿namespace Calculator;
+
+using System.ComponentModel;
+
+/// <summary>
+/// A class that implements a calculator with 5 operations: '+', '-', '*', '/', '+/-'.
+/// </summary>
+public class Calculator : INotifyPropertyChanged
+{
+    private string _display = "0";
+
+    /// <summary>
+    /// String representation of a number that is displayed.
+    /// </summary>
+    public string Display
+    {
+        get => _display;
+
+        private set
+        {
+            _display = value;
+            NotifyPropertyChanged();
+        }
+    }
+
+    private string _previousValue = "0";
+
+    private char _currentOperation = ' ';
+
+    private States _currentState = States.NumberTyping;
+
+    private const double Delta = 0.0001;
+    
+    private enum States
+    {
+        NumberTyping,
+        Operation,
+        Error,
+    }
+
+    /// <summary>
+    /// A data binding event that synchronizes the linked data at the time of change.
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void NotifyPropertyChanged(string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    
+    /// <summary>
+    /// Add an number to the calculator.
+    /// </summary>
+    /// <param name="number"> The number must be a digit. </param>
+    /// <exception cref="ArgumentException"></exception>
+    public void AddNumberInCalculator(char number)
+    {
+        if (!char.IsDigit(number))
+        {
+            throw new ArgumentException();
+        }
+
+        switch (_currentState)
+        {
+            case States.Error:
+            case States.NumberTyping:
+                if (Display is "0" or "Error")
+                {
+                    Display = number.ToString();
+                }
+                else
+                {
+                    Display += number;
+                }
+
+                _currentState = States.NumberTyping;
+                break;
+
+            case States.Operation:
+                _previousValue = Display;
+                Display = number.ToString();
+                _currentState = States.NumberTyping;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Add an operation to the calculator.
+    /// </summary>
+    /// <param name="operation"> The operation can be "+ - * /". </param>
+    /// <exception cref="ArgumentException"></exception>
+    public void AddOperationInCalculator(char operation)
+    {
+        if (!"+-*/".Contains(operation))
+        {
+            throw new ArgumentException();
+        }
+
+        switch (_currentState)
+        {
+            case States.NumberTyping:
+                if (_currentOperation == ' ')
+                {
+                    _previousValue = Display;
+                    _currentOperation = operation;
+                    _currentState = States.Operation;
+                }
+                else
+                {
+                    try
+                    {
+                        Display = Calculation().ToString();
+                    }
+                    catch (Exception e) when (e is ArgumentException or DivideByZeroException)
+                    {
+                        ClearCalculator();
+                        Display = "Error";
+                        _currentState = States.Error;
+                        return;
+                    }
+
+                    _previousValue = Display;
+                    _currentOperation = operation;
+                    _currentState = States.Operation;
+                }
+                break;
+
+            case States.Operation:
+                _currentOperation = operation;
+                break;
+
+            case States.Error:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Calculate the current expression in the calculator.
+    /// </summary>
+    public void Calculate()
+    {
+        double result = 0;
+
+        switch (_currentState)
+        {
+            case States.Error:
+            case States.NumberTyping:
+                if (_currentOperation != ' ')
+                {
+                    try
+                    {
+                        result = Calculation();
+                    }
+                    catch (Exception e) when (e is ArgumentException or DivideByZeroException)
+                    {
+                        ClearCalculator();
+                        Display = "Error";
+                        _currentState = States.Error;
+                        return;
+                    }
+
+                    Display = result.ToString();
+                    _currentOperation = ' ';
+                    _currentState = States.NumberTyping;
+                }
+                break;
+
+            case States.Operation:
+                try
+                {
+                    result = Calculation();
+                }
+                catch (Exception e) when (e is ArgumentException or DivideByZeroException)
+                {
+                    ClearCalculator();
+                    Display = "Error";
+                    _currentState = States.Error;
+                    return;
+                }
+
+                Display = result.ToString();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Change the sign of the current value.
+    /// </summary>
+    public void ChangeSign()
+    {
+        if (_currentState != States.Error && Display != "0")
+        {
+            if (Display[0] == '-')
+            {
+                Display = Display[1..];
+            }
+            else
+            {
+                Display = "-" + Display;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Clear the calculator.
+    /// </summary>
+    public void ClearCalculator()
+    {
+        Display = "0";
+        _previousValue = "0";
+        _currentOperation = ' ';
+        _currentState = States.NumberTyping;
+    }
+
+    private double Calculation()
+    {
+        switch (_currentOperation)
+        {
+            case '+':
+                return double.Parse(_previousValue) + double.Parse(Display);
+
+            case '-':
+                return double.Parse(_previousValue) - double.Parse(Display);
+            
+            case '*':
+                return double.Parse(_previousValue) * double.Parse(Display);
+            
+            case '/':
+                if (Math.Abs(double.Parse(Display)) < Delta)
+                {
+                    throw new DivideByZeroException();
+                }
+                return double.Parse(_previousValue) / double.Parse(Display);
+
+            default: throw new ArgumentException();
+        }
+    }
+}
